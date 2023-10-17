d9e9fabdd99864602083412ae304da81474a00ae
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-10-16 21:38:29 -0700

    styles animations, and modal bg

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
+// Project Modals
 const modals = document.querySelectorAll('[data-modal]');
 
 modals.forEach(function (trigger) {
@@ -16,3 +17,38 @@ modals.forEach(function (trigger) {
         })
     })
 })
+
+
+// ANIMATIONS
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-slide-up');
+        } else {
+            entry.target.classList.remove('show-slide-up');
+        }
+    })
+})
+
+const blurElements = document.querySelectorAll('.blur-hidden')
+const slideElements = document.querySelectorAll('.slide-hidden');
+const oppositeSlideElements = document.querySelectorAll('.slide-opposite-hidden');
+const slideUpElements = document.querySelectorAll('.slide-up-hidden');
+
+blurElements.forEach((element) => observer.observe(element))
+slideElements.forEach((element) => observer.observe(element))
+oppositeSlideElements.forEach((element) => observer.observe(element))
+slideUpElements.forEach((element) => observer2.observe(element))
+
